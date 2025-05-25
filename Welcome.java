8dccab357ac0a0df03c7dda8c65f2079bd89efdb
minseo <limminsoe08@gmail.com>
2025-05-25 21:00:25 +0900

    feat: 인사 메시지 변수 추가

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package MainMenu;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		String greeting = "Welcome to Shopping Mall";
+        String tagline = "Welcome to Book Market!";
+	}
+
+}
